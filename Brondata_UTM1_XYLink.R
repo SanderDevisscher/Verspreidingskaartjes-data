d825850492fe1db7dc937f3956efff0bd11df1cb
SanderDevisscher <sander.devisscher@inbo.be>
2016-10-24 16:28:57 +0200

    Gedetailleerd per soort toegevoegd

--- Brondata_UTM1_XYLink.R
+++ Brondata_UTM1_XYLink.R
@@ -4,6 +4,7 @@ setwd(dir = "C://Users/sander_devisscher/Google Drive/EU_IAS/T0/Verspreidingskaa
 
 library(dplyr)
 library(foreign)
+library(ggplot2)
 
 Brondata <- read.csv("C:/Users/sander_devisscher/Google Drive/EU_IAS/T0/invasive-t0-occurrences/data/processed/invasive_EU_listed_and_considered_with_joins.tsv", header=TRUE)
 UTM1 <- read.csv2("C:/Users/sander_devisscher/Google Drive/EU_IAS/T0/Verspreidingskaartjes data/UTM1.csv")
@@ -11,7 +12,7 @@ UTM1 <- read.csv2("C:/Users/sander_devisscher/Google Drive/EU_IAS/T0/Verspreidin
 Brondata_Ruw <- Brondata
 Brondata_Redux <- Brondata[c("euConcernStatus", "gis_utm1_code", "gbifapi_acceptedScientificName", "year")]
 
-####Opsplitsen#### Volgens eulist status
+####Opsplitsen Volgens eulist status####
 EuConc <- subset(Brondata_Redux, euConcernStatus == "listed")
 EuPrep <- subset(Brondata_Redux, euConcernStatus == "under preparation")
 EuCons <- subset(Brondata_Redux, euConcernStatus == "under consideration")
@@ -65,7 +66,85 @@ remove(UTM1_TAG)
 #Sometimes there are errors in the dataset
 #to enable to check the data from a single species 
 
-soorten <- Brondata
+soorten <- unique(Brondata$gbifapi_acceptedScientificName)
+print(soorten)
+soort <- "Vespa velutina Lepeletier, 1836" #Choose the species to be examined
+soortcode <- substr(soort, 1, 3)
+fname <- paste("C://Users/sander_devisscher/Google Drive/EU_IAS/T0/Verspreidingskaartjes data/Speciesdata/", soortcode, ".dbf", sep="")
+
+temp <- subset(Brondata, gbifapi_acceptedScientificName == soort)
+
+write.csv2(temp, fname)
+write.dbf(temp, fname)
+
+remove(temp)
+remove(soorten)
+remove(soort)
+remove(fname)
+remove(soortcode)
+
+####Gedetaileerd per soort####
+
+soorten <- unique(EuConc$gbifapi_acceptedScientificName)
+print(soorten)
+soort <- "Hydrocotyle ranunculoides L. fil." #Choose the species to be examined
+soortcode <- substr(soort, 1, 3)
+fname <- paste("C://Users/sander_devisscher/Google Drive/EU_IAS/T0/Verspreidingskaartjes data/Speciesdata/", soortcode, ".dbf", sep="")
+
+temp <- subset(EuConc, gbifapi_acceptedScientificName == soort)
+
+#Tijdelijke tussenstap
+Jaren <- c("2014","2015")
+tempexport <- subset(temp, year == Jaren)
+write.dbf(tempexport, fname)
+#Einde tijdelijke tussenstap
+
+n=0
+temp3 <- data.frame(x=soort)
+temp4 <- data.frame()
+Jaren <- unique(temp$year)
+for(j in Jaren){
+  temp2 <- subset(temp, year == j)
+  Hokken <- unique(temp2$gis_utm1_code)
+  for(h in Hokken){
+    n <- n + 1
+  }
+  temp3$hokken <- n
+  temp3$year <- j
+  temp3$soort <- soort
+  n = 0
+  temp4 <- rbind(temp4, temp3)
+}
+
+Brondata_Soort <- temp4
+Brondata_Soort$x <- NULL
+
+remove(temp)
+remove(temp2)
+remove(temp3)
+remove(temp4)
+
+remove(j)
+remove(h)
+remove(n)
+
+remove(Hokken)
+remove(Jaren)
+
+#Relevante data voor grafieken selecteren
+
+Jaren <- c("2014","2015","2016")
+
+GRA_Data <- subset(Brondata_Soort, year == Jaren)
+GRA_Data$year <- as.factor(GRA_Data$year)
+
+plot <- ggplot(GRA_Data, aes(x=year, y=hokken))
+plot <- plot + geom_bar(stat="identity")
+plot <- plot + scale_x_discrete(expand = c(0, 0)) + scale_y_continuous(limits=c(0,NA),expand = c(0, 0))
+print(plot)
+
+
+
 
 
 
